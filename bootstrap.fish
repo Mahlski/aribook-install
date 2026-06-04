5ef1655f97321cdf797a77f3a5242d70b6a83fdd
Guido <105006166+Mahlski@users.noreply.github.com>
2026-06-04 15:49:43 +0200

    bootstrap: add mail pkgs (isync/msmtp/notmuch)
    Mirror email stack from vault ari-base-pkg.md — P1 send (msmtp) + P2
    read/index (isync/notmuch); already installed manually, now scripted.

--- bootstrap.fish
+++ bootstrap.fish
@@ -22,8 +22,8 @@ set packages \
     dunst fastfetch fd file-roller firefox fuzzel fzf github-cli gimp gnupg grim \
     hyprcaffeine hypridle hyprland hyprlock hyprpaper hyprpolkitagent hyprshutdown \
     inetutils \
-    intel-media-driver jq kitty less libnotify libreoffice-still libreoffice-still-nl \
-    libva-utils lua-language-server mesa nodejs noto-fonts noto-fonts-cjk noto-fonts-emoji npm nwg-look \
+    intel-media-driver isync jq kitty less libnotify libreoffice-still libreoffice-still-nl \
+    libva-utils lua-language-server mesa msmtp nodejs notmuch noto-fonts noto-fonts-cjk noto-fonts-emoji npm nwg-look \
     obsidian openssh pacman-contrib pass pavucontrol pcmanfm pinentry pipewire \
     pipewire-alsa pipewire-jack pipewire-pulse pkgfile playerctl powertop \
     python-pipx qbz-bin qt5-wayland qt6-wayland ripgrep rsync slurp socat stow \
